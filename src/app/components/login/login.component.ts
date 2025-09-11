efafe3425621827eeefac031d7a76b5f4f1dc8cf
Ammar Omer <amar@livestreamlines.com>
2025-09-11 19:53:09 +0400

    Revamp login component UI with modern design elements, including a new gradient background, updated layout, and enhanced Microsoft login button. Removed legacy video background and streamlined component logic for improved performance and responsiveness.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit, ElementRef, AfterViewInit, ViewChild, HostListener } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';  // Import CommonModule for *ngIf
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../services/auth.service';
 import { HeaderService } from '../../services/header.service';
-import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-login',
@@ -14,28 +13,16 @@ import { Capacitor } from '@capacitor/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, AfterViewInit {
+export class LoginComponent implements OnInit {
   loginError: string | null = null;
-  isMobileView: boolean = false;
-  isIOS: boolean = false;
-  
-  @ViewChild('backgroundVideo', { static: true }) videoElement!: ElementRef<HTMLVideoElement>;
-
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.checkScreenSize();
-  }
 
   constructor(
     private authService: AuthService,
     private router: Router, 
-    private headerService: HeaderService) {
-      this.isIOS = Capacitor.getPlatform() === 'ios';
-    }
+    private headerService: HeaderService) {}
 
     ngOnInit(): void {
       this.headerService.showHeaderAndSidenav = false;
-      this.checkScreenSize();
 
       // Handle Microsoft login redirect
       this.authService.handleMicrosoftLogin().subscribe({
@@ -52,19 +39,6 @@ export class LoginComponent implements OnInit, AfterViewInit {
       });
     }
   
-    private checkScreenSize() {
-      this.isMobileView = this.isIOS || window.innerWidth <= 768;
-    }
-  
-    ngAfterViewInit(): void {
-      if (!this.isMobileView && this.videoElement && this.videoElement.nativeElement) {
-        this.videoElement.nativeElement.muted = true;
-        this.videoElement.nativeElement.play(); // Ensures the video starts playing muted
-      } 
-    } 
-
- 
-
   // Microsoft Login
   onMicrosoftLogin(): void {
     this.loginError = null;
